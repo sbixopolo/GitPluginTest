e7d2938f3188364e3de47e73f7bb55b923ec37b4
yg <yg@hotmail.com>
2017-03-15 16:22:01 +0100

    Une nouvelle modification dans la branche secondaire.

--- src/com/youness/tests/ClasseTest.java
+++ src/com/youness/tests/ClasseTest.java
@@ -1,5 +1,23 @@
 package com.youness.tests;
 
 public class ClasseTest {
-private String nom;
+	private String nom;
+	private String nouvelleVariable = null;
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getNouvelleVariable() {
+		return nouvelleVariable;
+	}
+
+	public void setNouvelleVariable(String nouvelleVariable) {
+		this.nouvelleVariable = nouvelleVariable;
+	}
+
 }
